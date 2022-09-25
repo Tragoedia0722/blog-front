6022cfc40377b156e75e7aba376c6858946defa1
Tragoedia <tragedywr@gmail.com>
2022-09-25 22:50:32 +0800

    首页左侧Main组件

---
+++ components/main/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classes from "./Main.module.css";
+import {Col, Row} from "antd";
+
+const Main = (props) => {
+    return (
+        <div>
+            <Row className={classes.main} type={"flex"} justify="center">
+                <Col className={classes.left} xs={24} sm={24} md={16} lg={18} xl={14}>
+                    {props.left}
+                </Col>
+                <Col className={classes.right} xs={0} sm={0} md={7} lg={5} xl={4}>
+                    {props.right}
+                </Col>
+            </Row>
+        </div>
+    );
+};
+
+export default Main;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,82 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Head from 'next/head'
 import Header from "../components/header/Header";
+import Main from "../components/main/Main"
+import {List} from "antd";
+import classes from "../components/main/Main.module.css";
+import {CalendarOutlined, FolderOutlined} from "@ant-design/icons";
 
-const Home = () => (
-    <>
-        <Head>
-            <title>Home</title>
-        </Head>
+const Home = () => {
+    const [dataSource, setDataSource] = useState([
+            {
+                title: '标题一',
+                date: '2022-09-25',
+                type: '记录',
+                context: '特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈'
+            },
+            {
+                title: '标题二',
+                date: '2022-09-25',
+                type: '记录',
+                context: '特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈'
+            },
+            {
+                title: '标题三',
+                date: '2022-09-25',
+                type: '记录',
+                context: '特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈'
+            },
+            {
+                title: '标题四',
+                date: '2022-09-25',
+                type: '记录',
+                context: '特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈'
+            },
+            {
+                title: '标题五',
+                date: '2022-09-25',
+                type: '记录',
+                context: '特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈特拉戈'
+            }
+        ]
+    )
 
-        <Header/>
-    </>
-)
+    const left = <List
+        itemLayout={"vertical"}
+        dataSource={dataSource}
+        renderItem={item => (
+            <List.Item>
+                <div className={classes.title}>{item.title}</div>
+                <div className={classes.icon}>
+                    <span>
+                        <CalendarOutlined/>&nbsp;{item.date}
+                    </span>
+                    &nbsp;&nbsp;
+                    <span>
+                        <FolderOutlined/>&nbsp;{item.type}
+                    </span>
+                </div>
+                <div className={classes.context}>
+                    {item.context}
+                </div>
+            </List.Item>
+        )}/>
+
+    const right = <div>
+
+    </div>
+
+    return (
+        <>
+            <Head>
+                <title>Home</title>
+            </Head>
+
+            <Header/>
+            <Main left={left} right={right}/>
+        </>
+    )
+}
+
 
 export default Home
